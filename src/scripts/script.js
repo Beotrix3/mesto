ce24c5121723416fa3b116c0329199abc843aa3d
beotrix <beotrix3@mail.ru>
2021-05-08 22:15:52 +0300

    mesto

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,30 +1,167 @@
-let openPopupButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closePopupButton = document.querySelector('.popup__close-button');
-let savePopupButton = document.querySelector('.popup__save-button');
+const openEditPopupButton = document.querySelector('.profile__edit-button');
+const openAddPopupButton = document.querySelector('.profile__add-button');
+const elementTitle = document.querySelector('.element__title');
+const elementImage = document.querySelector('.element__image');
+const popup = document.querySelector('.popup');
+const popupEdit = document.querySelector('.popup_edit-profile');
+const popupAdd = document.querySelector('.popup_element_add');
+const popupImage = document.querySelector('.popup_type_image');
+const popupLargeImage = document.querySelector('.popup__image');
+const popupCaption = document.querySelector('.popup__caption');
+const closeEditPopupButton = popupEdit.querySelector('.popup__close-button');
+const closeAddPopupButton = popupAdd.querySelector('.popup__close-button');
+const closeImagePopupButton = popupImage.querySelector('.popup__close-button');
+const formElement = document.querySelector('.popup__container');
+const formFigure = document.querySelector('.popup__figure');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+const popupName = formElement.querySelector('.popup__info_type_name');
+const popupDescription = formElement.querySelector('.popup__info_type_description');
 
-function togglePopup(evt) {
-  evt.preventDefault();
-  popup.classList.toggle('popup_opened');
-  popupName.value = profileName.textContent;
-  popupDescription.value = profileDescription.textContent;
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
 }
 
-openPopupButton.addEventListener('click', togglePopup);
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
 
-closePopupButton.addEventListener('click', togglePopup);
+openEditPopupButton.addEventListener('click', function() {
+  openPopup(popupEdit);
+  popupName.value = profileName.textContent;
+  popupDescription.value = profileDescription.textContent;
+});
 
-let formElement = document.querySelector('.popup__container');
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
-let popupName = document.querySelector('.popup__info_type_name');
-let popupDescription = document.querySelector('.popup__info_type_description');
+closeEditPopupButton.addEventListener('click', function () {
+  closePopup(popupEdit);
+});
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  popup.classList.toggle('popup_opened');
+  popupEdit.classList.toggle('popup_opened');
   profileName.textContent = popupName.value;
   profileDescription.textContent = popupDescription.value;
+  closePopup(popupEdit);
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+openAddPopupButton.addEventListener('click', function() {
+  openPopup(popupAdd);
+});
+
+closeAddPopupButton.addEventListener('click', function () {
+  closePopup(popupAdd);
+});
+
+closeImagePopupButton.addEventListener('click', function() {
+  closePopup(popupImage);
+});
+
+// --------------------------------
+
+const initialCards = [
+  {
+    name: 'Бергамо',
+    link: 'https://images.unsplash.com/photo-1597425922106-4b9353c68f33?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1391&q=80'
+  },
+  {
+    name: 'Флоренция',
+    link: 'https://images.unsplash.com/photo-1476362174823-3a23f4aa6d76?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    name: 'Милан',
+    link: 'https://images.unsplash.com/photo-1512204925985-f52390a87fda?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+  },
+  {
+    name: 'Венеция',
+    link: 'https://images.unsplash.com/photo-1534113414509-0eec2bfb493f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
+  },
+  {
+    name: 'Верона',
+    link: 'https://images.unsplash.com/photo-1567285272491-66350595f88b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=726&q=80'
+  },
+  {
+    name: 'Кампителло-ди-Фасса',
+    link: 'https://images.unsplash.com/photo-1605609476793-3015923b4be1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=613&q=80'
+  }
+];
+
+const elementTemplate = document.querySelector('#element-template').content;
+const elements = document.querySelector('.elements');
+
+initialCards.forEach(function(element){
+  const newElement = elementTemplate.querySelector('.element').cloneNode(true);
+  const elementDeleteButton = newElement.querySelector('.element__delete-button');
+  const likeButton = newElement.querySelector('.element__button');
+  const largeImage = newElement.querySelector('.element__image');
+
+  newElement.querySelector('.element__title').textContent = element.name;
+  newElement.querySelector('.element__title').alt = element.name;
+  newElement.querySelector('.element__image').src = element.link;
+
+  elements.append(newElement);
+
+  elementDeleteButton.addEventListener('click', function deleteElement(evt){
+    evt.target.closest('.element').remove();
+  });
+
+  likeButton.addEventListener('click', function() {
+    likeButton.classList.toggle('element__button_active');
+  });
+
+  largeImage.addEventListener('click', () => {
+    imagePopupHandler(element);
+  });
+});
+
+const createElementButton = document.querySelector('.popup__save-button_add');
+
+const formAddElement = document.querySelector('.popup__container_add');
+const popupTitle = formAddElement.querySelector('.popup__info_type_title');
+const popupLink = formAddElement.querySelector('.popup__info_type_link');
+
+function createElement(evt) {
+  evt.preventDefault();
+  popupAdd.classList.toggle('popup_opened');
+  const titleValue = popupTitle.value;
+  const linkValue = popupLink.value;
+  closePopup(popupAdd);
+
+  const newElement = elementTemplate.querySelector('.element').cloneNode(true);
+  const elementDeleteButton = newElement.querySelector('.element__delete-button');
+  const likeButton = newElement.querySelector('.element__button');
+  const largeImage = newElement.querySelector('.element__image');
+
+  newElement.querySelector('.element__title').textContent = titleValue;
+  newElement.querySelector('.element__title').alt = titleValue;
+  newElement.querySelector('.element__image').src = linkValue;
+
+  elements.prepend(newElement);
+
+  elementDeleteButton.addEventListener('click', function deleteElement(evt){
+    evt.target.closest('.element').remove();
+  });
+
+  likeButton.addEventListener('click', function () {
+    likeButton.classList.toggle('element__button_active');
+  });
+
+  largeImage.addEventListener('click', () => {
+    imagePopupHandler(linkValue,titleValue);
+  });
+}
+
+function imagePopupHandler (element) {
+  popupImage.classList.toggle('popup_opened');
+  popupLargeImage.src = element.link;
+  popupСaption.textContent = element.name;
+}
+
+function imagePopupHandler (linkValue,titleValue) {
+  popupImage.classList.toggle('popup_opened');
+  popupLargeImage.src = linkValue;
+  popupCaption.textContent = titleValue;
+}
+
+formAddElement.addEventListener('submit', createElement);
