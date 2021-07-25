56f1c49093a9371317452320902ab4524fb825e1
Alexey Grebnev <beotrix3@mail.ru>
2021-07-25 12:57:03 +0300

    mesto

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
-import '../pages/index.css'
-
-import Card from '../components/Card.js' 
-import FormValidator from '../components/FormValidator.js'
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
 import Section from '../components/Section.js';
 import UserInfo from '../components/UserInfo.js';
 import PopupWithForm from '../components/PopupWithForm.js';
